a08679baab42963e455a4eea707aab989803f663
porteriverson <live4soccer02@icloud.com>
2025-02-05 09:52:30 -0700

    Transferred over the code that was broken from the other file

--- Mission5/wwwroot/js/site.js
+++ Mission5/wwwroot/js/site.js
@@ -2,3 +2,13 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+$(document).ready(function()
+{
+    $("#btnSubmit").click(function () {
+        let perWeek = parseInt($("#perWeek").val());
+        let progLength = parseInt($("#progLength").val());
+        let cost = perWeek * progLength * 25;
+        alert("Total cost: $" + cost);
+    });
+});
+
